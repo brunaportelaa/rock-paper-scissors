df124ef533f65325369e4592f28939c662d19c36
brunaportelaa <brunaporteladj@gmail.com>
2024-04-27 11:22:01 -0300

    Created logic for comparing bot results with user results and display it to the user interface

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Grab value inserted by user
 document.getElementById('user-choice').addEventListener('submit', e => {
     e.preventDefault()
+    let displayBotChoice = document.getElementById('displayBotChoice')
+    let displayResult = document.getElementById('displayResult')
+    let displayUserPoints = document.getElementById('displayUserPoints')
+    let displayBotPoints = document.getElementById('displayBotPoints')
+    let result = document.getElementById('result')
     let select = document.getElementById('option')
     let option = select.value
     console.log(`User chose ${option}`)
@@ -12,19 +17,46 @@ document.getElementById('user-choice').addEventListener('submit', e => {
     //Compare value of the bot with value of user
     if (botChoice === 0) {
         botChoice = 'rock'
-        console.log('Bot chose rock')
+        displayBotChoice.textContent = 'Bot chose rock'
+
+        if (option === 'rock') {
+            displayResult.textContent = "Its a tie"
+        } else if (option === 'paper') {
+            displayResult.textContent = 'You win'
+        } else {
+            displayResult.textContent = 'You lose'
+        }
+
+
     } else if (botChoice === 1) {
         botChoice = 'paper'
-        console.log('Bot chose paper')
+        displayBotChoice.textContent = 'Bot chose paper'
+        
+        if (option === 'rock') {
+            displayResult.textContent = "You lose"
+        } else if (option === 'paper') {
+            displayResult.textContent = 'Its a tie'
+        } else {
+            displayResult.textContent = 'You win'
+        }
+
     } else if (botChoice === 2) {
         botChoice = 'scissors'
-        console.log('Bot chose scissors')
+        displayBotChoice.textContent = 'Bot chose scissors'
+        
+        if (option === 'rock') {
+            displayResult.textContent = "You win"
+        } else if (option === 'paper') {
+            displayResult.textContent = 'You lose'
+        } else {
+            displayResult.textContent = 'Its a tie'
+        }
+
     } else {
-        console.log ('Oops... something went wrong')
+        displayBotChoice.textContent = 'Oops... something went wrong'
     }
 
+    //Display results
+    result.classList.remove('hidden')
+
 })
-
-
-
-//Display results
